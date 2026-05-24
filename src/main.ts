583644bac143b75a13ebd3e25da06b13e72fb2eb
Rahul R <rahulramachandran432@gmail.com>
2026-05-24 12:38:29 +0530

    feature: add global exception handler

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http.exception.filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,10 @@ async function bootstrap() {
   app.useGlobalInterceptors(
     new ResponseInterceptor()
   )
+
+  app.useGlobalFilters(
+  new HttpExceptionFilter(),
+);
 
   await app.listen(process.env.PORT ?? 3000);
 }
